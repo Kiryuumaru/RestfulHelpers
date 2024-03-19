540027330f98f795906816b7e9ec0d63e093d57f
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-03-19 15:49:25 +0800

    Added `NukeBuildHelpers`

---
+++ build/RestfulHelpersTestEntry.cs
@@ -0,0 +1,30 @@
+﻿using Nuke.Common.IO;
+using Nuke.Common.Tools.DotNet;
+using NukeBuildHelpers;
+using NukeBuildHelpers.Attributes;
+using NukeBuildHelpers.Enums;
+using NukeBuildHelpers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _build;
+
+public class RestfulHelpersTestEntry : AppTestEntry<Build>
+{
+    public override bool RunParallel => false;
+
+    public override RunsOnType RunsOn => RunsOnType.Ubuntu2204;
+
+    public override Type[] AppEntryTargets => [typeof(RestfulHelpersEntry)];
+
+    public override void Run()
+    {
+        DotNetTasks.DotNetClean(_ => _
+            .SetProject(NukeBuild.Solution.RestfulHelpers_UnitTest));
+        DotNetTasks.DotNetTest(_ => _
+            .SetProjectFile(NukeBuild.Solution.RestfulHelpers_UnitTest));
+    }
+}
