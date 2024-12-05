525d8c0d0f875ce0030a0f3c74c947a8fdaee0fd
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2024-12-05 14:45:29 +0800

    Native AOT Compatible (#198)

--- RestfulHelpers/Common/Internals/Message.cs
+++ RestfulHelpers/Common/Internals/Message.cs
@@ -2,5 +2,6 @@
 
 internal class Message
 {
+    internal const string RequiresDynamicCode = $"Serializable objects must preserve all its required types when trimming is enabled.";
     internal const string RequiresUnreferencedCodeMessage = $"Serializable objects must preserve all its required types when trimming is enabled";
 }
