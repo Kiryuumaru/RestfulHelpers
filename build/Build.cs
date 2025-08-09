36db04ef3f485046367ec37f9737942a0476ab25
Clynt Neiko Rupinta <31558769+Kiryuumaru@users.noreply.github.com>
2025-08-10 01:25:07 +0800

    Updated CICD (#336)

--- build/Build.cs
+++ build/Build.cs
@@ -58,6 +58,11 @@ public class Build : BaseNukeBuildHelpers
                 DotNetTasks.DotNetClean(_ => _
                     .SetProject(projectPath));
                 DotNetTasks.DotNetTest(_ => _
+                    .SetProcessAdditionalArguments(
+                        "--logger \"GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true\" " +
+                        "-- " +
+                        "RunConfiguration.CollectSourceInformation=true " +
+                        "DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencovere ")
                     .SetProjectFile(projectPath));
             }
         });
